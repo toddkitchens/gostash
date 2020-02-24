7c0a32ab071cc985b555bb2b54ea085371e5ee69
Todd Kitchens <todd.kitchens@gmail.com>
2020-02-23 21:39:38 -0500

    More practice with TYPE

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 var y = 42
-var a = `James said, "shaken 
-
-not 
-
+var a = `James said, "shaken
+not
 		stirred"`
 
 func main() {
 	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 	fmt.Println(a)
-	//fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", a)
 }
